6d28e592de45317482ea7bc4f7949fb9915c5714
artembaskoca@Gmail.com <artembaskoca@gmail.com>
2026-04-12 23:52:53 -0400

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUrl: 'https://101486489-comp-3133-assignment02-backend-h4f9lxy5d.vercel.app/graphql'
+  graphqlUrl: 'https://101486489-comp-3133-assignment02-ba.vercel.app/graphql'
 };
