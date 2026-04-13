39dd66845fab41a8ddb34758124490b55712abe4
artembaskoca@Gmail.com <artembaskoca@gmail.com>
2026-04-12 23:50:52 -0400

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUrl: 'https://your-backend-url.onrender.com/graphql',
+  graphqlUrl: 'https://101486489-comp-3133-assignment02-backend-h4f9lxy5d.vercel.app/graphql'
 };
